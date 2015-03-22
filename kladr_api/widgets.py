2fb9294fc96e745a04289adae517c52ce68482e8
Fish <okfish@yandex.ru>
2015-03-22 17:00:23 +0300

    Move per-input options to data-kladr-options attr, which should be picked by initKladrForm function on document ready.
    Switch to dev-branch of kladr-jsclient due to bugs with unverified fields. Values are stored now but marked as errors.
    
    Disable original kladrZip plugin on postcode field.

--- kladr_api/widgets.py
+++ kladr_api/widgets.py
@@ -12,7 +12,8 @@ class KladrWidget(forms.TextInput):
     Subclass and define widget_type. It can be ('city', 'region', 'zip' etc)
     see https://kladr-api.ru/integration/
     """
-    # subclasses should override this
+    # subclasses should override this props
+    jscode = ''
     widget_type = None 
     options = {
         'url' : KLADR_API_URL,
@@ -22,9 +23,6 @@ class KladrWidget(forms.TextInput):
     def start_jscript(self, id=None):
         jscode = """<script type="text/javascript">
                         $(document).ready(function() {\n""" 
-        if id:
-            jscode += """$inp = $(document.getElementById('%(id)s'));
-                        $parent = $inp.parents("form");\n""" % { 'id' : id }
         return jscode
     
     def close_jscript(self):
@@ -32,7 +30,6 @@ class KladrWidget(forms.TextInput):
 
     def render_jscript(self, id=None, inner_js=None):
         return self.start_jscript(id) + inner_js + self.close_jscript()
-    
     
     def get_options(self):
         """
@@ -48,28 +45,45 @@ class KladrWidget(forms.TextInput):
                    
     def render(self, name, value, attrs=None):
          
-        jscode = ''
+        jscode = self.jscode
         options = self.get_options()
         attrs = self.build_attrs(attrs)
+        attrs['autocomplete'] = 'off'
+        attrs['data-kladr-type'] = self.widget_type
+        
+        if self.widget_type:
+            id_ = attrs.get('id', None)
+            parent_id_ = attrs.get('parent_id', None)
+            
+            if parent_id_:
+                #options['verify'] = True
+                options['parentId'] = parent_id_
+                if self.widget_type == 'street':
+                    options['parentType'] = 'city'
+                elif self.widget_type == 'city':
+                    #options['parentType'] = 'region'
+                    options['verify'] = True
+                    options.pop('parentId')
+                elif self.widget_type == 'building':
+                    options['parentType'] = 'street'    
+ 
+            attrs['data-kladr-options'] = json.dumps(options)
+            if jscode:
+                jscode = self.render_jscript(id_, jscode) 
+        
         s = unicode(super(KladrWidget, self).render(name, value, attrs))
-        
-        if not self.widget_type:
-            return s
-        
-        id_ = attrs.get('id', None)
-        
-        jscode += "$inp.kladr({ parentInput : $parent });\n" 
-        jscode += "$inp.kladr(" + json.dumps(options) + ");\n" 
-        jscode = self.render_jscript(id_, jscode)
-             
         s += jscode
         return mark_safe(s)
 
     class Media:
-        js = ('kladr_api/js/jquery.kladr.min.js', 
+        js = (#'kladr_api/js/jquery.kladr.min.js',
+              'kladr_api/js/core.js', #switched to dev-branch
+              'kladr_api/js/kladr.js',
+               'kladr_api/js/common.js',
               )
         css = {
                'all': ('kladr_api/css/jquery.kladr.min.css',
+                       'kladr_api/css/kladr_api.css',
                        )
                }
         
@@ -80,6 +94,18 @@ class KladrRegionWidget(KladrWidget):
     Uses Kladr-api.ru JQuery plugin for suggestions.
     """
     widget_type = 'region'
+
+    def render(self, name, value, attrs=None):
+         
+        attrs = self.build_attrs(attrs)
+        attrs['disabled'] = 'true'
+        s = unicode(super(KladrRegionWidget, self).render(name, value, attrs))
+        return mark_safe(s)
+    
+    def get_options(self):
+        options = super(KladrRegionWidget, self).get_options()
+        options['verify'] = True
+        return options
 
 class KladrCityWidget(KladrWidget):
     """
@@ -116,20 +142,28 @@ class KladrPostcodeWidget(KladrWidget):
     get remaining fields such as region,city,street corresponding to
     entered postcode. 
     
-    FIX: There are some issues, works strange.
+    FIXME: There are some issues, works strange. Temporarily original plugin
+    is switched off due to useless...
+    
     
     Uses Kladr-api.ru JQuery plugin for suggestions.
     
     """
     widget_type = 'zip'
     
-    def render(self, name, value, attrs=None):
-         
-        attrs = self.build_attrs(attrs)
-        attrs['autocomplete'] = 'off'
-        s = unicode(super(KladrWidget, self).render(name, value, attrs))
-        
-        id_ = attrs.get('id', None)
-        s += self.render_jscript(id_, "$inp.kladrZip($parent);")
-
-        return mark_safe(s)    
+    def get_options(self):
+        options = super(KladrPostcodeWidget, self).get_options()
+        options['verify'] = False
+        return options
+#     
+#     def render(self, name, value, attrs=None):
+#          
+#         attrs = self.build_attrs(attrs)
+#         attrs['autocomplete'] = 'off'
+#         
+#         s = unicode(super(KladrWidget, self).render(name, value, attrs))
+#         
+#         id_ = attrs.get('id', None)
+#         #s += self.render_jscript(id_, "$inp.kladrZip($parent);")
+# 
+#         return mark_safe(s)    
